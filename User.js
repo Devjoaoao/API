2148e27212084a9b418d1f2c8a2e0dc4e248048b
Devjoaoao <singlejoao0@gmail.com>
2025-03-15 16:56:37 +0100

    Add files via upload

--- User.js
+++ User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    approved: { type: Boolean, default: false } // Adiciona o campo de aprovação
+  name:     { type: String, required: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  approved: { type: Boolean, default: false } // Usuários são criados como não aprovados
 });
 
 module.exports = mongoose.model('User', userSchema);
